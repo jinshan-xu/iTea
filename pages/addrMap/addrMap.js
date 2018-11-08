1b97dc37f84d7ccf27b7b84edd3fab399bed6bb3
jinshan-xu <907596538@qq.com>
2018-11-08 23:56:35 +0800

    新增地图以及推荐位置

--- pages/addrMap/addrMap.js
+++ pages/addrMap/addrMap.js
@@ -1,66 +1,159 @@
 // pages/addrMap/addrMap.js
+var QQMapWX = require('../../libs/qqmap-wx-jssdk.js');
+var qqmapsdk;
+var map;
 Page({
-
-  /**
-   * 页面的初始数据
-   */
   data: {
-
+    polyline: [],
+    location: {
+      latitude: 22.72174,
+      longitude:114.06301
+    },
+    markers: [{
+      latitude: 22.72174,
+      longitude:114.06301,
+      id: 1,
+      name: '当前选中位置',    
+    }],
+    recPos: [],
+    selectedIdx: 0  // 选中的位置序号
   },
 
-  /**
-   * 生命周期函数--监听页面加载
-   */
   onLoad: function (options) {
-
+    // 初始化创建地图对象
+    this.mapCtx = wx.createMapContext('myMap'); 
+    // 获取授权   
+    wx.getSetting({
+      success(res) {               
+        if (!res.authSetting['scope.userLocation']) {
+          wx.authorize({
+            scope: 'scope.userLocation',
+            success () {            
+              console.log("ok");
+            }
+          })
+        }
+      }
+    });    
+    // 获取腾讯地图控制对象
+    this.qqMap = new QQMapWX({
+      key: '4RABZ-ANNHS-RBYOC-6IZL2-DCYCT-HJB5G'
+    })
+    this.moveToLocation();
+    console.log(11);
   },
 
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
+  // 拖动地图时移动 marker
+  handleChange(e){        
+    var _this = this;    
+    // 只有当拖动改变视图 / 拖动结束 时才会执行
+    if(e.type == "end" && e.causedBy == "drag"){
+      this.getRecPos();
+    }      
   },
 
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
+  initRecPosi(){
+    // 地图渲染完成时获取位置推荐列表
+    this.getRecPos();    
   },
 
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
+  getCenterLocation(){  // 获取地图中心位置经纬度
+    this.mapCtx.getCenterLocation({      
+      success(res){
+        return res;
+      }    
+    })
   },
 
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
+  moveToLocation(){
+    // 将地图中心定位到 当前手机地理位置
+    this.mapCtx.moveToLocation();  
   },
 
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
+  getLocation(){     // 获取当前位置经纬度
+    wx.getLocation({
+      type: 'gcj02',
+      success(res) {        
+        return res;       
+      }
+    })
   },
 
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
+  changePosition(){   // 修改地图中心经纬度
+    var location = this.data.location;
+    var { longitude, latitude } = this.data.location;
+    longitude = parseFloat(longitude - 0.001).toFixed(5);
+    latitude = parseFloat(latitude - 0.001).toFixed(5);
+    location.longitude = longitude;
+    location.latitude = latitude;
+    this.setData({
+      location
+    });
   },
 
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
+  getRecPos(){   // 获取推荐地址
+    var _this = this;
+    this.mapCtx.getCenterLocation({      
+      success(res){        
+        // 获取此时位置的推荐地址          
+        _this.qqMap.reverseGeocoder({
+          location: {
+            latitude: res.latitude,
+            longitude: res.longitude
+          },
+          success: function(res) {
+            // 推荐地点 到此处地标          
+            var addrRef = res.result.address_reference;
+            var landMark = addrRef.landmark_11 ||   addrRef.landmark_l2;                    
+            if(typeof landMark == 'object'){
+              // 查询
+              _this.search(landMark.title);
+            }                    
+          }
+        });
+      }    
+    });
+    
+  },
 
+  getSuggestion(sug){  // 关键字输入推荐
+    var _this = this;
+    this.qqMap.getSuggestion({
+      keyword: sug,
+      policy: 1,
+      success(res){           
+        //console.log(res);
+        _this.setData({
+          recPos: res.data
+        });        
+      } 
+    })
+  },
+
+  search(location){  // 关键字查询返回模糊匹配结果    
+    var _this = this;
+    this.qqMap.search({      
+      address_format: 'short',
+      keyword: location,
+      page_size: 10,
+      success(res){        
+        // console.log(res);
+        _this.setData({
+          recPos: res.data
+        });        
+      }
+    })
+  },
+
+  selectPos(e){  // 选中位置
+    console.log(e.target.dataset);
+    var idx = e.target.dataset.id;
+    console.log(idx);
+    if(idx !== undefined){      
+      this.setData({
+        selectedIdx: idx
+      })
+    }
   }
+
 })
--- pages/test/test.js
+++ pages/test/test.js
@@ -1,73 +1,61 @@
-// pages/test/test.js
 Page({
-
-  /**
-   * 页面的初始数据
-   */
   data: {
-    obj: {
-      name: {
-        a: 1
-      },
-      age: {
-        a: 20
+    latitude: 23.099994,
+    longitude: 113.324520,
+    markers: [{
+      id: 1,
+      latitude: 23.099994,
+      longitude: 113.324520,
+      name: 'T.I.T 创意园'
+    }],
+    covers: [{
+      latitude: 23.099994,
+      longitude: 113.344520,
+      iconPath: '../../assets/images/location/location.png'
+    }, {
+      latitude: 23.099994,
+      longitude: 113.304520,
+      iconPath: '../../assets/images/location/location.png'
+    }]
+  },
+  onReady: function (e) {
+    this.mapCtx = wx.createMapContext('myMap')
+  },
+  getCenterLocation: function () {
+    this.mapCtx.getCenterLocation({
+      success: function(res){
+        console.log(res.longitude)
+        console.log(res.latitude)
       }
-    }
+    })
   },
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-    
+  moveToLocation: function () {
+    this.mapCtx.moveToLocation()
   },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
+  translateMarker: function() {
+    this.mapCtx.translateMarker({
+      markerId: 1,
+      autoRotate: true,
+      duration: 1000,
+      destination: {
+        latitude:23.10229,
+        longitude:113.3345211,
+      },
+      animationEnd() {
+        console.log('animation end')
+      }
+    })
   },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+  includePoints: function() {
+    this.mapCtx.includePoints({
+      padding: [10],
+      points: [{
+        latitude:23.10229,
+        longitude:113.3345211,
+      }, {
+        latitude:23.00229,
+        longitude:113.3345211,
+      }]
+    })
   }
 })
